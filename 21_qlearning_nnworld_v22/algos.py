1c3f044f3528a61b974bccc0168cfecc7da44112
Tuan Nguyen <tnguyen9210@gmail.com>
2023-10-04 15:54:31 -0700

    update new gridworld setting

--- 21_qlearning_nnworld_v22/algos.py
+++ 21_qlearning_nnworld_v22/algos.py
@@ -76,12 +76,12 @@ def eps_greedy_policy(action_muhats, action_nvisits, state, num_depths, eps):
     num_actions = len(action_muhats)
     num_actions_half = int(num_actions/2)
     elems = re.findall(r'\d+', state)
-    agent_depth = int(elems[0])
-    agent_width = int(elems[1])
-    if agent_depth in [num_depths-1, num_depths-2]:
-        eps_greedy_action = 0
+    state_depth = int(elems[0])
+    state_width = int(elems[1])
+    if state_depth in [0, num_depths-1]:
+    #     eps_greedy_action = 0
         
-    elif agent_depth == 0:
+    # elif state_depth == 0:
         action_muhats = copy.deepcopy(action_muhats)
         action_muhats[action_nvisits == 0] = inf2
         greedy_action = greedy_policy(action_muhats)
@@ -90,7 +90,7 @@ def eps_greedy_policy(action_muhats, action_nvisits, state, num_depths, eps):
         action_probs[greedy_action] += 1 - eps
         eps_greedy_action = np.random.choice(num_actions, 1, p=action_probs)[0]
         
-    elif agent_width < num_actions_half:        
+    elif state_width < num_actions_half:        
         action_muhats_half = copy.deepcopy(action_muhats[:num_actions_half])
         action_muhats_half[action_nvisits[:num_actions_half] == 0] = inf2
         greedy_action = greedy_policy(action_muhats_half)
@@ -100,7 +100,7 @@ def eps_greedy_policy(action_muhats, action_nvisits, state, num_depths, eps):
         eps_greedy_action = np.random.choice(
             num_actions_half, 1, p=action_probs_half)[0]
         
-    elif agent_width >= num_actions_half:
+    elif state_width >= num_actions_half:
         action_muhats_half = copy.deepcopy(action_muhats[num_actions_half:])
         action_muhats_half[action_nvisits[num_actions_half:] == 0] = inf2
         greedy_action = greedy_policy(action_muhats_half)
@@ -131,14 +131,13 @@ def q_learning(env, num_actions, num_steps_train,
             range(num_steps_train), desc="train q_learning", disable=tdqm_disable):
 
         # print(f"\n-> i_step = {i_step}")
-        # print(f"state = {cur_state}")
+        # print(f"cur_state = {cur_state}")
         # choose the action a_t using epsilon greedy policy
         nvisits = np.sum(Q_nvisits[cur_state]) + 1
         # eps = 1.0/np.sqrt(nvisits)
         eps = eps_sched_fn(nvisits)
         action = eps_greedy_policy(
-            Q_table[cur_state], Q_nvisits[cur_state], cur_state,
-            args["num_depths"], eps)
+            Q_table[cur_state], Q_nvisits[cur_state], cur_state, num_depths, eps)
         # if cur_state == "[1 2]":
         #     print(Q_table[cur_state])
 
@@ -150,15 +149,23 @@ def q_learning(env, num_actions, num_steps_train,
 
         if not terminated:
             elems = re.findall(r'\d+', new_state)
-            agent_depth = int(elems[0])
-            agent_width = int(elems[1])
-            if agent_depth in [num_depths-1]:
-                Q_est = Q_table[new_state][0]
-            elif agent_depth == 0:
-                Q_est = np.max(Q_table[new_state])
-            elif agent_width < num_actions_half:
+            new_state_depth = int(elems[0])
+            new_state_width = int(elems[1])
+            # print(new_state_depth)
+            if new_state_depth is [0, num_depths-1]:
+                print(new_state_depth)
+                stop
+            # action_muhats = copy.deepcopy(Q_table[new_state])
+            # action_nvisits = Q_nvisits[new_state]
+            # action_muhats[action_nvisits == 0] = -np.inf
+            # if new_state_width < num_actions_half:
+            #     Q_est = np.max(action_muhats[:num_actions_half])
+            # elif new_state_width >= num_actions_half:
+            #     Q_est = np.max(action_muhats[num_actions_half:])
+            # Q_est = Q_est if np.sum(action_nvisits) != 0 else 0.0
+            if new_state_width < num_actions_half:
                 Q_est = np.max(Q_table[new_state][:num_actions_half])
-            elif agent_width >= num_actions_half:
+            elif new_state_width >= num_actions_half:
                 Q_est = np.max(Q_table[new_state][num_actions_half:])
             
             td_error = reward + gamma*Q_est - Q_table[cur_state][action]
@@ -180,8 +187,10 @@ def q_learning(env, num_actions, num_steps_train,
             # print(f"reset_state = {cur_state}")
         else:
             cur_state = new_state
-            
-        # stats.append((i_step + 1, episode_reward/(i_step+1), np.max(Q_table[start_state])))
+
+        # action_muhats = copy.deepcopy(Q_table[start_state])
+        # action_nvisits = Q_nvisits[start_state]
+        # action_muhats[action_nvisits == 0] = -np.inf
         Q_start_est = np.max(Q_table[start_state])
         stats.append((np.sum(Q_nvisits[start_state]),
                       reward, Q_start_est))
@@ -271,7 +280,10 @@ def double_estimator(DQ_table, new_state, Q_idx):
 def weightedms_q_learning(
         env, num_actions, num_steps_train,
         gamma, lr_sched_fn, eps_sched_fn, tdqm_disable, args=None):
-        
+
+    num_depths = args["num_depths"]
+    num_actions_half = int(num_actions/2)
+    
     # keep track of useful statistics
     stats = []
     num_data = args["weightedms_num_data"]
@@ -294,15 +306,32 @@ def weightedms_q_learning(
         nvisits = np.sum(Q_nvisits[cur_state]) + 1
         # eps = 1.0/np.sqrt(nvisits)
         eps = eps_sched_fn(nvisits)
-        action = eps_greedy_policy(Q_table[cur_state], Q_nvisits[cur_state], eps)
+        action = eps_greedy_policy(
+            Q_table[cur_state], Q_nvisits[cur_state], cur_state, num_depths, eps)
 
         new_state, reward, terminated, truncated, info = env.step(action)
         new_state = f"{new_state}"
 
         if not terminated:
-            # compute Q_est with weighted estimator
-            Q_est = weightedms_estimator(
-                Q_table, Q2_table, Q_sigmahats, new_state, num_actions, num_data)
+            elems = re.findall(r'\d+', new_state)
+            new_state_depth = int(elems[0])
+            new_state_width = int(elems[1])
+
+            if new_state_width < num_actions_half:
+                action_muhats = copy.deepcopy(Q_table[new_state][:num_actions_half])
+                action_sigmahats = Q_sigmahats[new_state][:num_actions_half]
+                action_sigmahats[action_sigmahats < 1e-5] = 1e-5
+                
+                Q_est = weightedms_estimator(
+                    action_muhats, action_sigmahats, num_actions_half, num_data)
+
+            elif new_state_width >= num_actions_half:
+                action_muhats = copy.deepcopy(Q_table[new_state][num_actions_half:])
+                action_sigmahats = Q_sigmahats[new_state][num_actions_half:]
+                action_sigmahats[action_sigmahats < 1e-5] = 1e-5
+                
+                Q_est = weightedms_estimator(
+                    action_muhats, action_sigmahats, num_actions_half, num_data)            
             
             # compute td_target
             td_target = reward + gamma*Q_est
@@ -315,7 +344,7 @@ def weightedms_q_learning(
         Q_table[cur_state][action] = (1-lr)*Q_table[cur_state][action] + lr*td_target
         Q2_table[cur_state][action] = (1-lr)*Q2_table[cur_state][action] + lr*td_target**2
 
-        if Q_nvisits[cur_state][action] > 1:
+        if Q_nvisits[cur_state][action] >= 1:
             weights_var[cur_state][action] = \
                 (1-lr)**2*weights_var[cur_state][action] + lr**2
             n = 1.0/weights_var[cur_state][action]
@@ -330,9 +359,12 @@ def weightedms_q_learning(
         else:
             cur_state = new_state
 
-        # stats.append((i_step + 1, episode_reward/(i_step+1), np.max(Q_table[start_state])))
+        action_muhats = copy.deepcopy(Q_table[start_state])
+        action_sigmahats = Q_sigmahats[start_state]
+        action_sigmahats[action_sigmahats < 1e-5] = 1e-5
+        
         Q_start_est = weightedms_estimator(
-            Q_table, Q2_table, Q_sigmahats, start_state, num_actions, num_data)
+            action_muhats, action_sigmahats, num_actions, num_data)
         stats.append((np.sum(Q_nvisits[start_state]),
                       reward, Q_start_est))
 
@@ -342,17 +374,18 @@ def weightedms_q_learning(
     return Q_table_ary, Q_nvisits_ary, stats
     # return Q_table, stats
 
-def weightedms_estimator(Q_table, Q2_table, Q_sigmahats, est_state, num_actions, num_data):
-    cur_muhats = Q_table[est_state]
-    cur_sigmahats = Q_sigmahats[est_state]
-    cur_sigmahats[cur_sigmahats < 1e-4] = 1e-4
+def weightedms_estimator(
+        action_muhats, action_sigmahats, num_actions, num_data):
+    # cur_muhats = Q_table[est_state]
+    # cur_sigmahats = Q_sigmahats[est_state]
+    # cur_sigmahats[cur_sigmahats < 1e-4] = 1e-4
 
     # sample 
-    cur_muhats_mat = matlib.repmat(cur_muhats, num_data, 1)
-    cur_sigmahats_mat = matlib.repmat(cur_sigmahats, num_data, 1)
+    action_muhats_mat = matlib.repmat(action_muhats, num_data, 1)
+    action_sigmahats_mat = matlib.repmat(action_sigmahats, num_data, 1)
     eps_mat = np.random.randn(num_data, num_actions)
 
-    samples = cur_muhats_mat + cur_sigmahats_mat*eps_mat
+    samples = action_muhats_mat + action_sigmahats_mat*eps_mat
     samples_max_idxes = np.argmax(samples, 1)
 
     # compute probs 
@@ -362,16 +395,17 @@ def weightedms_estimator(Q_table, Q2_table, Q_sigmahats, est_state, num_actions,
     probs = probs/num_data
 
     # compute Q_est 
-    Q_est = np.dot(probs, cur_muhats)
+    Q_est = np.dot(probs, action_muhats)
     # print(Q_est)
     # stop
     return Q_est
 
-
-
 def haver2_q_learning(env, num_actions, num_steps_train,
                gamma, lr_sched_fn, eps_sched_fn, tdqm_disable, args=None):
 
+    num_depths = args["num_depths"]
+    num_actions_half = int(num_actions/2)
+    
     # get params
     # action_sigma = args["action_sigma"]
     haver_alpha = args["haver_alpha"]
@@ -395,23 +429,48 @@ def haver2_q_learning(env, num_actions, num_steps_train,
             range(num_steps_train), desc="train q_learning", disable=tdqm_disable):
 
         # print(f"\n-> i_step = {i_step}")
-        # print(f"state = {state}")
+        # print(f"cur_state = {cur_state}")
         # choose the action a_t using epsilon greedy policy
         nvisits = np.sum(Q_nvisits[cur_state]) + 1
         # eps = 1.0/np.sqrt(nvisits)
         eps = eps_sched_fn(nvisits)
-        action = eps_greedy_policy(Q_table[cur_state], Q_nvisits[cur_state], eps)
-
+        action = eps_greedy_policy(
+            Q_table[cur_state], Q_nvisits[cur_state], cur_state, num_depths, eps)
+        
         new_state, reward, terminated, truncated, info = env.step(action)
         new_state = f"{new_state}"
+        # print(f"nvisits = {nvisits}")
+        # print(f"eps = {eps}")
         # print(f"action = {action}")
-        # print(f"reward = {reward}")
+        # print(f"reward = {reward:0.2f}")
         # print(f"new_state = {new_state}")
 
         if not terminated:
-            Q_est = haver2_estimator(
-                Q_table, Q_sigmahats, Q_nvisits, new_state, num_actions,
-                haver_alpha, haver_delta, haver_const, lr_sched_fn)
+            elems = re.findall(r'\d+', new_state)
+            new_state_depth = int(elems[0])
+            new_state_width = int(elems[1])
+
+            if new_state_width < num_actions_half:
+                action_muhats = copy.deepcopy(Q_table[new_state][:num_actions_half])
+                action_sigmahats = Q_sigmahats[new_state][:num_actions_half]
+                action_sigmahats[action_sigmahats < 1e-5] = 1e-5
+                action_nvisits = Q_nvisits[new_state][:num_actions_half]
+                action_weights_var = weights_var[new_state][:num_actions_half]
+                
+                Q_est = haver2_estimator(
+                    action_muhats, action_sigmahats, action_nvisits, action_weights_var,
+                    num_actions_half, haver_alpha, haver_delta, haver_const, lr_sched_fn)
+                
+            elif new_state_width >= num_actions_half:
+                action_muhats = copy.deepcopy(Q_table[new_state][num_actions_half:])
+                action_sigmahats = Q_sigmahats[new_state][num_actions_half:]
+                action_sigmahats[action_sigmahats < 1e-5] = 1e-5
+                action_nvisits = Q_nvisits[new_state][num_actions_half:]
+                action_weights_var = weights_var[new_state][num_actions_half:]
+                
+                Q_est = haver2_estimator(
+                    action_muhats, action_sigmahats, action_nvisits, action_weights_var,
+                    num_actions_half, haver_alpha, haver_delta, haver_const, lr_sched_fn)
             
             # td_error = reward + gamma*Q_est - Q_table[cur_state][action]
             td_target = reward + gamma*Q_est
@@ -427,7 +486,7 @@ def haver2_q_learning(env, num_actions, num_steps_train,
         Q2_table[cur_state][action] = \
             (1-lr)*Q2_table[cur_state][action] + lr*td_target**2
 
-        if Q_nvisits[cur_state][action] >  1:
+        if Q_nvisits[cur_state][action] >=  1:
             weights_var[cur_state][action] = \
                 (1-lr)**2*weights_var[cur_state][action] + lr**2
             n = 1.0/weights_var[cur_state][action]
@@ -444,10 +503,15 @@ def haver2_q_learning(env, num_actions, num_steps_train,
         else:
             cur_state = new_state
             
-        # stats.append((i_step + 1, episode_reward/(i_step+1), np.max(Q_table[start_state])))
+        action_muhats = copy.deepcopy(Q_table[start_state])
+        action_sigmahats = Q_sigmahats[start_state]
+        action_sigmahats[action_sigmahats < 1e-5] = 1e-5
+        action_nvisits = Q_nvisits[start_state]
+        action_weights_var = weights_var[start_state]
+                
         Q_start_est = haver2_estimator(
-            Q_table, Q_sigmahats, Q_nvisits, start_state, num_actions,
-            haver_alpha, haver_delta, haver_const, lr_sched_fn)
+                    action_muhats, action_sigmahats, action_nvisits, action_weights_var,
+                    num_actions, haver_alpha, haver_delta, haver_const, lr_sched_fn)
         # Q_start_est = np.max(Q_table[start_state])
         stats.append((np.sum(Q_nvisits[start_state]),
                       reward, Q_start_est))
@@ -466,29 +530,32 @@ def haver2_q_learning(env, num_actions, num_steps_train,
     # return Q_table, stats
 
 
-def haver2_estimator(Q_table, Q_sigmahats, Q_nvisits, est_state, num_actions,
-           haver_alpha, haver_delta, haver_const, lr_sched_fn):
-    action_muhats = copy.deepcopy(Q_table[est_state])
-    action_sigmahats = Q_sigmahats[est_state]
-    action_sigmahats[action_sigmahats < 1e-4] = 1e-4
-    action_nvisits = Q_nvisits[est_state]
-    # print(action_muhats)
-            
+def haver2_estimator(
+        action_muhats, action_sigmahats, action_nvisits, action_weights_var,
+        num_actions, haver_alpha, haver_delta, haver_const, lr_sched_fn):
+
+    # print(num_actions)
+    # print(len(action_muhats))
     action_muhats[action_nvisits == 0] = -np.inf
     action_max_idx = np.argmax(action_muhats)
     action_max_muhat = action_muhats[action_max_idx]
     action_max_sigmahat = action_sigmahats[action_max_idx]
     action_max_nvisits = action_nvisits[action_max_idx]
+    action_max_weights_var = action_weights_var[action_max_idx]
     # print(action_muhats)
     # print(action_sigmahats)
     # print(action_nvisits)
+    # print(action_weights_var)
+
 
     Q_est_sum = 0
     Q_est_cnt = 0
     for i in range(num_actions):
         if action_nvisits[i] != 0:
-            avg = action_max_sigmahat**2/action_max_nvisits \
-                + action_sigmahats[i]**2/action_max_nvisits
+            # avg = action_max_sigmahat**2/action_max_nvisits \
+            #     + action_sigmahats[i]**2/action_nvisits[i]
+            avg = action_max_sigmahat**2*action_max_weights_var \
+                + action_sigmahats[i]**2*action_weights_var[i]
             # avg = action_max_sigmahat**2 + action_sigmahats[i]**2
             thres = haver_const*np.sqrt(avg*np.log(num_actions**haver_alpha/haver_delta))
             # print(i)
@@ -496,8 +563,10 @@ def haver2_estimator(Q_table, Q_sigmahats, Q_nvisits, est_state, num_actions,
             # print(action_max_muhat)
             # print(action_muhats[i])
             if action_max_muhat - action_muhats[i] <= thres:
-                Q_est_sum += action_muhats[i]
-                Q_est_cnt += 1
+                # Q_est_sum += action_muhats[i]*action_nvisits[i]
+                # Q_est_cnt += action_nvisits[i]
+                Q_est_sum += action_muhats[i]/action_weights_var[i]
+                Q_est_cnt += 1.0/action_weights_var[i]
             
     Q_est = Q_est_sum/Q_est_cnt if Q_est_cnt != 0 else 0.0
     # print(f"Q_est_sum = {Q_est_sum}")
